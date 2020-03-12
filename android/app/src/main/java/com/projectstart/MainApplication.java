dc9c2c962bb7c4d3ef4953850b163d80d911c67d
Truong Phan Qui <quitp@tinhvan.com>
2020-03-12 11:39:16 +0700

    add notification for ios

--- android/app/src/main/java/com/projectstart/MainApplication.java
+++ android/app/src/main/java/com/projectstart/MainApplication.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
-import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
